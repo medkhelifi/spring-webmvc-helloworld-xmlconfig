5aef5e09832ed0078459761a34bf8289f9a54133
Mohamed KHELIFI <mohamedchrif.khelifi@gmail.com>
2019-07-14 19:29:06 +0200

    clean master branch

--- src/main/java/com/medkhelifi/tutorials/swhx/controllers/IndexController.java
+++ src/main/java/com/medkhelifi/tutorials/swhx/controllers/IndexController.java
@@ -9,6 +9,6 @@ public class IndexController {
 
     @RequestMapping(value = "")
     public String index(){
-        return "Hello World from deploy/heroku";
+        return "Hello World";
     }
 }
