123d88da511bf717eda8592b86a088ed1f858363
Mohamed KHELIFI <mohamedchrif.khelifi@gmail.com>
2019-07-14 14:14:11 +0200

    setting up spring webmvc hello world

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-webmvc-helloworld-xmlconfig
 The simplest spring webmvc application using the XML configuration
+
+## Build
+`$ mvn clean package` 
--- src/main/java/com/medkhelifi/tutorials/swhx/controllers/IndexController.java
+++ src/main/java/com/medkhelifi/tutorials/swhx/controllers/IndexController.java
@@ -1,7 +1,5 @@
 package com.medkhelifi.tutorials.swhx.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
